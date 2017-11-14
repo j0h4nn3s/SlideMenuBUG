3615dedf56d6cb2b83e3ed66e76f6165ef36f4d4
Johannes Hubert <johannes.hubert@rtfc.de>
2017-11-14 17:52:59 +0100

    added hack for normal iPhone size

--- MenuTest/AppDelegate.swift
+++ MenuTest/AppDelegate.swift
@@ -10,23 +10,27 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     var slideMenuController: SlideMenuController?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.main.bounds)
         
         let mainViewController = MainController()
         let leftViewController = MenuController()
         
-        let navigationController = UINavigationController(rootViewController: mainViewController)
+        let navigationController = NC(navigationBarClass: NB.self, toolbarClass: nil)
+        navigationController.viewControllers = [mainViewController]
         let btnMenu = UIBarButtonItem(title: "Menu", style: .done, target: self, action: #selector(openMenu))
         navigationController.topViewController?.navigationItem.rightBarButtonItem = btnMenu
         
+        navigationController.navigationBar.isTranslucent = false
+        navigationController.view.backgroundColor = .red
+        
         self.slideMenuController = SlideMenuController(mainViewController: navigationController, leftMenuViewController: leftViewController)
         self.window?.rootViewController = self.slideMenuController
-    
+        
         self.window?.makeKeyAndVisible()
         
         return true
@@ -35,29 +39,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     @objc private func openMenu() {
         self.slideMenuController?.openLeft()
     }
+}
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+extension UIBarPosition: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .any: return "any"
+        case .bottom: return "bottom"
+        case .top: return "top"
+        case .topAttached: return "attached"
+        }
     }
+}
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+class NB: UINavigationBar {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let backgroundView = self.subviews[0]
+        backgroundView.frame = CGRect(x: 0, y: -20, width: 375, height: 64)
     }
+}
 
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+class NC: UINavigationController {
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        print(self.navigationBar.barPosition)
     }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
---
+++ MenuTest/NavigationController.swift
@@ -0,0 +1,16 @@
+//
+//  NavigationController.swift
+//  MenuTest
+//
+//  Created by Johannes Hubert on 14.11.17.
+//  Copyright © 2017 Johannes Hubert. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NavigationController: UINavigationController {
+    override var navigationBar: UINavigationBar {
+        
+    }
+}
